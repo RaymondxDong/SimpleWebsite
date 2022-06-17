b2e81b6e49ac605c639bad6d0a28bcc615c8b3ed
raymond <raymondxdong@gmail.com>
2022-06-17 00:22:57 -0400

    Init #1 simple website

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 
@@ -6,8 +6,8 @@ print(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
